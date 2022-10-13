b07057396b858dd7f107041107217d66c75290c2
Tran Thanh Tung <thanhtung.4work@gmail.com>
2022-10-13 09:47:01 +0700

    Add project files.

---
+++ QuanLyNhanSu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLyNhanSu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
