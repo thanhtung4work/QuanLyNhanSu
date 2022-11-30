1183ce5938ba0df12820c88c76f1050d2a06f992
Tran Thanh Tung <thanhtung.4work@gmail.com>
2022-11-30 10:10:20 +0700

    add báo cáo, update database, update page heading

---
+++ QuanLyNhanSu.Service/IEmployee.cs
@@ -0,0 +1,19 @@
+﻿using QuanLyNhanSu.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhanSu.Service
+{
+    public interface IEmployee
+    {
+        Task CreateAsync(Employee employee);
+        Employee GetById(int id);
+        Task UpdateAsync(int id);
+        Task DeleteAsync(int id);
+        
+        IEnumerable<Employee> GetAll();
+    }
+}
---
+++ QuanLyNhanSu.Service/Implementation/EmployeeService.cs
@@ -0,0 +1,56 @@
+﻿using QuanLyNhanSu.Entity;
+using QuanLyNhanSu.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhanSu.Service.NewFolder
+{
+    public class EmployeeService : IEmployee
+    {
+        private readonly QuanLyNhanSuDbContext _context;
+        public EmployeeService(QuanLyNhanSuDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(Employee employee)
+        {
+            await _context.Employees.AddAsync(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var employee = GetById(id);
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public Employee GetById(int id)
+        {
+            return _context.Employees.Where(employee => employee.Id == id).FirstOrDefault();
+        }
+
+        public async Task UpdateAsync(Employee employee)
+        {
+            _context.Employees.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(int id)
+        {
+            var employee = GetById(id);
+            _context.Employees.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
